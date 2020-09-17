d24029b891110d3fbf9af12d5aec6015baed11a7
max-cepelev <tsepelev_maksim@yahoo.com>
2020-09-17 17:19:31 +0500

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -90,4 +90,22 @@ $(document).ready(function(){
     formValidate('#consultation-form');
     formValidate('#consultation form');
     formValidate('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        }); 
+        return false;
+    });
+
 });
